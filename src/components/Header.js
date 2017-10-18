b5848907665e1144d1dd75b57a7d169ca1a902be
VicTomm <vtomm6575@gmail.com>
2017-10-18 14:40:52 -0500

    burger menu functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-const Header = () => (
-    <div class="hero-head">
-        <header class="navbar">
-            <div class="container">
-                <div class="navbar-brand">
-                    <Link className="navbar-item" to="/">
-                        <img src="/images/logoHeader.png" alt="Motiva" width="112" height="28" />
-                    </Link>
-                    <span class="navbar-burger burger" data-target="navbarMenuHeroC">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </span>
-                </div>
-                <div id="navbarMenuHeroC" class="navbar-menu">
-                    <div class="navbar-end">
-                        <NavLink to="/nosotrxs" className="navbar-item" activeClassName="is-active">Nosotrxs</NavLink>
-                        <NavLink to="/contacto" className="navbar-item" activeClassName="is-active">Contacto</NavLink>
+export class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isActive: false
+        }
+    }
+
+    handleBurgerMenu = () => {
+        this.setState((prevState) => ({
+            isActive: !prevState.isActive
+        }));
+    };
+
+    render() {
+        return (
+            <div className="hero-head">
+                <header className="navbar">
+                    <div className="container">
+                        <div className="navbar-brand">
+                            <Link className="navbar-item" to="/">
+                                <img src="/images/logoHeader.png" alt="Motiva" width="112" height="28" />
+                            </Link>
+                            <span onClick={this.handleBurgerMenu} className={"navbar-burger burger " + (this.state.isActive ? 'is-active' : '')} data-target="navbarMenuHeroC">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </div>
+                        <div id="navbarMenuHeroC" className={"navbar-menu " + (this.state.isActive ? 'is-active' : '')}>
+                            <div className="navbar-end">
+                                <NavLink to="/nosotrxs" className="navbar-item" activeClassName="is-active">Nosotrxs</NavLink>
+                                <NavLink to="/contacto" className="navbar-item" activeClassName="is-active">Contacto</NavLink>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </header>
             </div>
-        </header>
-    </div>
-);
+        );
+    }
+}
 
 export default Header;
