2ff28340e4c20266e8057556495d8de748be0923
VicTomm <vtomm6575@gmail.com>
2017-10-18 15:58:42 -0500

    added animation to Secretarias

--- src/components/SecretariaPage.js
+++ src/components/SecretariaPage.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { startSetSecretarias } from '../actions/actions';
 
 export class SecretariaPage extends React.Component {
   render() {
     return (
-      <div className="container">
-        <section className="hero">
-          <div class="hero-body">
-            <div class="has-text-centered">
-              <h1 class="title">
-                {this.props.secretaria.id}
-              </h1>
-              <hr />
-              <h2 class="subtitle">
-                Objetivo General:
+      <ReactCSSTransitionGroup
+        transitionName="motiva"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={true}
+        transitionLeave={false}>
+        <div className="container">
+          <section className="hero">
+            <div class="hero-body">
+              <div class="has-text-centered">
+                <h1 class="title">
+                  {this.props.secretaria.id}
+                </h1>
+                <hr />
+                <h2 class="subtitle">
+                  Objetivo General:
               </h2>
-              <p>{this.props.secretaria.objetivoG}</p>
-              <h2 class="subtitle">
-                Objetivos Particulares:
+                <p>{this.props.secretaria.objetivoG}</p>
+                <h2 class="subtitle">
+                  Objetivos Particulares:
               </h2>
-              <ol>
-                {this.props.secretaria.objetivosP.objetivoUno && <li>{this.props.secretaria.objetivosP.objetivoUno}</li>}
-                {this.props.secretaria.objetivosP.objetivoDos && <li>{this.props.secretaria.objetivosP.objetivoDos}</li>}
-                {this.props.secretaria.objetivosP.objetivoTres && <li>{this.props.secretaria.objetivosP.objetivoTres}</li>}
-                {this.props.secretaria.objetivosP.objetivoCuatro && <li>{this.props.secretaria.objetivosP.objetivoCuatro}</li>}
-                {this.props.secretaria.objetivosP.objetivoCinco && <li>{this.props.secretaria.objetivosP.objetivoCinco}</li>}
-                {this.props.secretaria.objetivosP.objetivoSeis && <li>{this.props.secretaria.objetivosP.objetivoSeis}</li>}
-                {this.props.secretaria.objetivosP.objetivoSiete && <li>{this.props.secretaria.objetivosP.objetivoSiete}</li>}
-              </ol>
+                <ol>
+                  {this.props.secretaria.objetivosP.objetivoUno && <li>{this.props.secretaria.objetivosP.objetivoUno}</li>}
+                  {this.props.secretaria.objetivosP.objetivoDos && <li>{this.props.secretaria.objetivosP.objetivoDos}</li>}
+                  {this.props.secretaria.objetivosP.objetivoTres && <li>{this.props.secretaria.objetivosP.objetivoTres}</li>}
+                  {this.props.secretaria.objetivosP.objetivoCuatro && <li>{this.props.secretaria.objetivosP.objetivoCuatro}</li>}
+                  {this.props.secretaria.objetivosP.objetivoCinco && <li>{this.props.secretaria.objetivosP.objetivoCinco}</li>}
+                  {this.props.secretaria.objetivosP.objetivoSeis && <li>{this.props.secretaria.objetivosP.objetivoSeis}</li>}
+                  {this.props.secretaria.objetivosP.objetivoSiete && <li>{this.props.secretaria.objetivosP.objetivoSiete}</li>}
+                </ol>
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
+          </section>
+        </div>
+      </ReactCSSTransitionGroup>
     );
   }
 };
